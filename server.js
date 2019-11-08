6e7dd64cb8317c0fe0ebdbb67453be49e566389d
Hector Donovan Rincon Couret <donovan@wizeline.com>
2019-11-08 10:04:00 -0600

    Add cors

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var tweetsRoutes = require('./api/routes/tweetRoutes'); //importing route
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+const cors = require('cors');
 
 var bodyParser = require('body-parser')
 var app = express()
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(cors());
 // parse application/json
 app.use(bodyParser.json())
 app.use('/api/tweets', tweetsRoutes);
